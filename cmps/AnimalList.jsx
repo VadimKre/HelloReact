ec45720cc2e2b94938668f9e54f000f101a591e6
VadimKre <vkreslavsky@gmail.com>
2025-08-26 13:23:32 +0300

    added <thead> to fix table warning

--- cmps/AnimalList.jsx
+++ cmps/AnimalList.jsx
@@ -8,11 +8,13 @@ export function AnimalList ({animalList}){
             <table className="animal-table">
                 {animalList.map( (animal, index) => {
                     return (
-                        <tr key={animal.type + 'tr' +index}>
-                            <td key={animal.type + "1"}>{animal.type}</td>
-                            <td key={animal.type + '2'}>{animal.count}</td>
-                            <td key={animal.type + '3'}><a href={"https://www.google.com/search?q=" + animal.type}> Search </a></td>
-                        </tr>
+                        <thead key={animal.type + 'thead' + index}>
+                            <tr key={animal.type + 'tr' +index}>
+                                <td key={animal.type + "1"}>{animal.type}</td>
+                                <td key={animal.type + '2'}>{animal.count}</td>
+                                <td key={animal.type + '3'}><a href={"https://www.google.com/search?q=" + animal.type}> Search </a></td>
+                            </tr>
+                        </thead>
                         )
                 })}
             </table>
